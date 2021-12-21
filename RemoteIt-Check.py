4049d788d82f5cf5722118e04d51b627ac05b793
jcmb <Geoffrey@Jcmbsoft.com>
2021-12-21 12:09:19 -0700

    Update RemoteIt-Check.py
    Add support for variable number of services, it was 3 before.
    
    Also add improved help.
    
    Removed the modules that were not used.

--- RemoteIt-Check.py
+++ RemoteIt-Check.py
@@ -1,37 +1,31 @@
 #!/usr/bin/env python3 -u
-import json
-import os
-import os.path
-from pprint import pprint
 import argparse
-import sys
+from pprint import pprint
+import os
 from datetime import datetime
-import urllib.parse
-import logging
 import csv
-import fileinput
-import glob
+import sys
 
-import cgitb
-#cgitb.enable()
-
-import collections
-
-from JCMBSoftPyLib import HTML_Unit
-import tempfile
 
 from RemoteIt import RemoteIt
 
 
+
 def get_args():
 
-   parser = argparse.ArgumentParser(fromfile_prefix_chars="@",description='Remote.It Account Summary.')
+   parser = argparse.ArgumentParser(fromfile_prefix_chars="@",description='Remote.It Check Device services.', epilog="""
+   Checks a device or a list of devides in a file to check the number of services.
+   When a file is provided using the Reg option, a .to_delete and a .to_fix file, based on the file name will be created.
+   The to_delete file contains a list of devices that do not have the full number of services
+   The to_fix file contains a list of devices that do not have the full number of services or was not in the account.
+   """)
 
    parser.add_argument("username", help="Account User name",)
    parser.add_argument("password", help="Account password",)
    parser.add_argument("key", help="Account developer key",)
+   parser.add_argument("--Services", help="Number of Services expected",type=int,default=3)
    parser.add_argument("--Device_ID", help="Device ID")
-   parser.add_argument("--Reg", help="Registration File, file spec to check", nargs='+', type=str)
+   parser.add_argument("--Reg", help="Registration file, file spec to check", nargs='+', type=str)
    parser.add_argument("--Verbose","-v", help="Verbose",action="store_true")
 
    parser = parser.parse_args()
@@ -42,6 +36,7 @@ def get_args():
    args["DEVICE_ID"]=parser.Device_ID
    args["Reg_File"]=parser.Reg
    args["Verbose"]=parser.Verbose
+   args["Services"]=parser.Services
    return (args)
 
 
@@ -59,10 +54,10 @@ def main():
 
    if args["DEVICE_ID"]:
       remotes=remoteIt.get_devices_by_name(args["DEVICE_ID"])
-      pprint(remotes)
+#      pprint(remotes)
       if remotes:
-         if len(remotes["devices"]) != 3:
-            print ("Error: {} only has {} instead of 3 services".format(args["DEVICE_ID"],len(remotes["devices"])))
+         if len(remotes["devices"]) != args["Services"]:
+            print ("Error: {} has {} instead of {} services".format(args["DEVICE_ID"],len(remotes["devices"]),args["Services"]))
       else:
          print ("Error: {} is not registered".format(args["DEVICE_ID"]))
 
@@ -96,9 +91,9 @@ def main():
    #         pprint(remotes)
             Records+=1
             Total_Records+=1
-            if len(remotes["devices"]) == 3:
+            if len(remotes["devices"]) == args["Services"]:
                if args["Verbose"]:
-                  sys.stderr.write ("OK: {} is registered with 3 services. From file {}\n".format(row[2],filename))
+                  sys.stderr.write ("OK: {} is registered with {} services. From file {}\n".format(row[2],args["Services"],filename))
             else:
                if len(remotes["devices"]) == 0:
                   sys.stderr.write ("Error: {} is not registered. From file {}\n".format(row[2],filename))
@@ -111,7 +106,7 @@ def main():
                      if device["servicetitle"] == "Bulk Service":
                         HW_ID=device["deviceaddress"]
 
-                  sys.stderr.write ("Error: {} only has {} instead of 3 services. HW_ID: {}. From file {}\n".format(row[2],len(remotes["devices"]),HW_ID,filename))
+                  sys.stderr.write ("Error: {} has {} instead of {} services. HW_ID: {}. From file {}\n".format(row[2],len(remotes["devices"]),args["Services"],HW_ID,filename))
                   to_fix_regwriter.writerow(row)
                   to_delete_regwriter.writerow(row)
 
